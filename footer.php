f9b30066aa4d094b39a1d6c99cc08d1846549baa
aandreluis <andreluisstn@gmail.com>
2021-12-21 20:58:43 -0300

    Feature: Correções no form e rotinas no cadastro/alteração de filmes

--- footer.php
+++ footer.php
@@ -18,7 +18,6 @@
 
 		<!-- Rodapé -->
 
-	<script type="text/javascript" src="js/script.js"></script>
 	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
 	</body>
 </html>
--- remove-filme.php
+++ remove-filme.php
@@ -7,6 +7,8 @@
 
 <?php 
     $id = $_GET["id"];
+    
+    removeImagemCapaUsuario($conexao, $id);
     removeFilme($conexao, $id);
     header("Location: listar-filmes.php?removido=true");//volta para a pagita listar-filmes.php com um valor GET = true
     die();
