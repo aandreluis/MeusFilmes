ed9b9293191865fdf1739f290c78a352e809c99f
aandreluis <andreluisstn@gmail.com>
2022-01-08 22:34:01 -0300

    Fix: Corrige bug de aparecer buscar e paginação quando não há  filmes

--- listar-filmes.php
+++ listar-filmes.php
@@ -71,147 +71,149 @@
     $resultado = mysqli_query($conexao, $query);
     ?>
     <section class="mt-3">
-    <div class="custom-home-container">
-        <div id="filmes-pg">
-            <h1 class="titulo-home">Filmes Cadastrados</h1>
-            <?php
-             if (!listaFilmes($conexao, $_SESSION["id-usuario"])) {//verifica se existe filmes para mostrar
-                ?>
-                <div class="row d-flex justify-content-center mt-3">
-                    <div class="col-md-12">
-                        <h5 class="paragrafo-home text-muted">Ops... Nenhum filme encontrado!</h5>
-                        <img class="mx-auto d-block mt-3" style="width: 50%;" src="img/searching.svg" alt="Nada encontrado" >
+        <div class="custom-home-container">
+            <div id="filmes-pg">
+                <h1 class="titulo-home">Filmes Cadastrados</h1>
+                <?php            
+                if(listaFilmes($conexao, $_SESSION["id-usuario"])) {//verifica se existe filmes para mostrar
+                    ?>
+                    <!-- Pesquisa -->
+                    <div class="row d-flex justify-content-center mt-3">
+                        <div class="col-6">
+                            <div class="form-text">Tente pesquisar o filme pelo seu nome. Exemplo: "Homem-Aranha"</div>
+                            <form class="d-flex mt-2" id="form-pesquisa" method="POST" action="">
+                                <input class="form-control" id="pesquisa" type="text" placeholder="Buscar filmes pelo nome" aria-label="Search">
+                            </form>
+                        </div>
                     </div>
-                </div> 
-                <?php
-            }
-            ?>
-            <!-- Pesquisa -->
-            <div class="row d-flex justify-content-center mt-3">
-                <div class="col-6">
-                    <div class="form-text">Tente pesquisar o filme pelo seu nome. Exemplo: "Homem-Aranha"</div>
-                    <form class="d-flex mt-2" id="form-pesquisa" method="POST" action="">
-                        <input class="form-control" id="pesquisa" type="text" placeholder="Buscar filmes pelo nome" aria-label="Search">
-                    </form>
-                </div>
-            </div>
-            <div class="resultado"><!-- Exibe os resultados da pesquisa por jquery --></div>
-            <!-- Pesquisa -->
+                    <div class="resultado"><!-- Exibe os resultados da pesquisa por jquery --></div>
+                    <!-- Pesquisa -->
 
-
-                <!-- Cards -->
-                <div id="card" class="row row-cols-1 row-cols-md-3 g-4 mt-2">
-                    <?php
+                    <!-- Card -->
+                    <div id="card" class="row row-cols-1 row-cols-md-3 g-4 mt-2">
+                        <?php
                         //While para pegar as informações do BD
                         while($filme = mysqli_fetch_array($resultado)) {
-                    ?>
-                    <!-- início card -->
-                    <div class="col">
-                        <div class="card text-white bg-dark border-card h-100 card-box">
-                    <?php
-                    if($filme["imagem"] == NULL || $filme["imagem"] == "") {
                         ?>
-                        <img src="img/capa-default.svg" class="card-img-top" alt="Capa do filme default" style="padding: 20px;">
-                        <?php
-                    } else {
-                        ?>
-                        <img src="img/capa-filmes/<?php echo $filme["imagem"] ?>" class="card-img-top" alt="Capa do filme <?php echo $filme["nome"]; ?>">     
-                        <?php
-                    }
-                    ?>
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $filme["nome"];?></h5>
-                            <p class="card-text">
-                                <b>Diretor:</b> <?php echo $filme["diretor"];?> <br>
-                                <b>Duração:</b> <?php echo date('H:i', strtotime($filme["duracao"]));?> <br>
-                                <b>Data Lançamento:</b> <?php echo date('d/m/Y', strtotime($filme["data_lancamento"]));?> <br>
-                                <b>Categoria:</b> <?php echo $filme["categoria_nome"];?> <br>
-                                <?php if($filme["assistido"] == '0') {
+                        <!-- início conteudo do card -->
+                        <div class="col">
+                            <div class="card text-white bg-dark border-card h-100 card-box">
+                                <?php
+                                if($filme["imagem"] == NULL || $filme["imagem"] == "") {
                                     ?>
-                                    <b>Já assistido</b>
-                                    <i class="bi bi-play-circle-fill"></i>
+                                    <img src="img/capa-default.svg" class="card-img-top" alt="Capa do filme default" style="padding: 20px;">
                                     <?php
-                                    } else {
+                                } else {
                                     ?>
-                                    <b>Nunca assistido</b>
-                                    <i class="bi bi-pause-circle-fill"></i>
+                                    <img src="img/capa-filmes/<?php echo $filme["imagem"] ?>" class="card-img-top" alt="Capa do filme <?php echo $filme["nome"]; ?>">     
                                     <?php
-                                    } ?> <br>
-                                <b>Descrição:</b> <?php echo $filme["descricao"];?>
-                            </p>
-                            <div class="card-footer">
-                                <div class="row">
-                                    <p class="card-subtitle text-center text-muted">Ações</p>
-                                </div>    
-                                <div class="row mt-2">
-                                    <a href="remove-filme.php?id=<?php echo $filme["id"];?>" class="btn btn-danger veiculos-btn d-block mx-auto"><i class="bi bi-trash"></i></a>
-                                    <a href="altera-filme-form.php?id=<?php echo $filme["id"];?>" class="btn btn-primary veiculos-btn d-block mx-auto"><i class="bi bi-gear-fill"></i></a>
-                                </div>       
+                                }
+                                ?>
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $filme["nome"];?></h5>
+                                    <p class="card-text">
+                                        <b>Diretor:</b> <?php echo $filme["diretor"];?> <br>
+                                        <b>Duração:</b> <?php echo date('H:i', strtotime($filme["duracao"]));?> <br>
+                                        <b>Data Lançamento:</b> <?php echo date('d/m/Y', strtotime($filme["data_lancamento"]));?> <br>
+                                        <b>Categoria:</b> <?php echo $filme["categoria_nome"];?> <br>
+                                        <?php 
+                                        if($filme["assistido"] == '0') {
+                                            ?>
+                                            <b>Já assistido</b>
+                                            <i class="bi bi-play-circle-fill"></i>
+                                            <?php
+                                        } else {
+                                            ?>
+                                            <b>Nunca assistido</b>
+                                            <i class="bi bi-pause-circle-fill"></i>
+                                            <?php
+                                        } ?> <br>
+                                        <b>Descrição:</b> <?php echo $filme["descricao"];?>
+                                    </p>
+                                    <div class="card-footer">
+                                        <div class="row">
+                                            <p class="card-subtitle text-center text-muted">Ações</p>
+                                        </div>    
+                                        <div class="row mt-2">
+                                            <a href="remove-filme.php?id=<?php echo $filme["id"];?>" class="btn btn-danger veiculos-btn d-block mx-auto"><i class="bi bi-trash"></i></a>
+                                            <a href="altera-filme-form.php?id=<?php echo $filme["id"];?>" class="btn btn-primary veiculos-btn d-block mx-auto"><i class="bi bi-gear-fill"></i></a>
+                                        </div>       
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        </div>
+                            <!-- fim do conteudo do card -->
+                        <?php
+                        } //fim while
+                        ?>
                     </div>
-                    <!-- fim card -->
-                <?php
-                } //fim while
-                ?>
-                </div>
-                <!-- Cards -->
+                        <!-- Card -->
 
-                <?php
-                // Faz uma nova seleção no banco de dados, desta vez sem LIMIT, 
-                // para pegarmos o número total de registros
-                $query = "select f.*, c.nome as categoria_nome from filmes as f join categoria as c on c.id = f.categoria_id where f.usuario_id = '{$_SESSION["id-usuario"]}'";
-                $resultado = mysqli_query($conexao, $query);//retorna um array de valores
+                    <?php
+                    // Faz uma nova seleção no banco de dados, desta vez sem LIMIT, 
+                    // para pegarmos o número total de registros
+                    $query = "select f.*, c.nome as categoria_nome from filmes as f join categoria as c on c.id = f.categoria_id where f.usuario_id = '{$_SESSION["id-usuario"]}'";
+                    $resultado = mysqli_query($conexao, $query);//retorna um array de valores
 
-                // Gera uma variável com o número total de registros no banco de dados
-                $total_registros = mysqli_num_rows($resultado);
+                    // Gera uma variável com o número total de registros no banco de dados
+                    $total_registros = mysqli_num_rows($resultado);
 
-                // Gera outra variável, desta vez com o número de páginas que será precisa. 
-                // O comando ceil() arredonda "para cima" o valor
-                $pags = ceil($total_registros / $qnt);
+                    // Gera outra variável, desta vez com o número de páginas que será precisa. 
+                    // O comando ceil() arredonda "para cima" o valor
+                    $pags = ceil($total_registros / $qnt);
 
-                // Número máximos de botões de paginação
-                $max_links = 10;
+                    // Número máximos de botões de paginação
+                    $max_links = 10;
 
-                // Exibe o primeiro link "primeira página", que não entra na contagem acima(3)
-                ?>
-                <br>
-                <nav id="pags" aria-label="Página listar filmes">
-                    <ul class="pagination justify-content-center">
-                        <li class="page-item"><a class="page-link" href="listar-filmes.php?p=1">Início</a></li>
-                <?php
-                // Cria um for() para exibir os 3 links antes da página atual
-                for($i = $p - $max_links; $i <= $p - 1; $i++) {
-                    // Se o número da página for menor ou igual a zero, não faz nada
-                    // (afinal, não existe página 0, -1, -2..)
-                    if($i <= 0) {
-                    //faz nada
-                    } else { // Se estiver tudo OK, cria o link para outra página
-                        ?>
-                        <li class="page-item"><a class="page-link" href="listar-filmes.php?p=<?php echo $i;?>"><?php echo $i;?></a></li>
-                        <?php
+                    // Exibe o primeiro link "primeira página", que não entra na contagem acima(3)
+                    ?>
+                    <br>
+                    <nav id="pags" aria-label="Página listar filmes">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item"><a class="page-link" href="listar-filmes.php?p=1">Início</a></li>
+                    <?php
+                    // Cria um for() para exibir os 3 links antes da página atual
+                    for($i = $p - $max_links; $i <= $p - 1; $i++) {
+                        // Se o número da página for menor ou igual a zero, não faz nada
+                        // (afinal, não existe página 0, -1, -2..)
+                        if($i <= 0) {
+                        //faz nada
+                        } else { // Se estiver tudo OK, cria o link para outra página
+                            ?>
+                            <li class="page-item"><a class="page-link" href="listar-filmes.php?p=<?php echo $i;?>"><?php echo $i;?></a></li>
+                            <?php
+                        }
                     }
-                }
-                ?>
-                <li class="page-item active"><a class="page-link" href="listar-filmes.php?p=<?php echo $p;?>"><?php echo $p;?></a></li>
-                <?php
-                // Cria outro for(), desta vez para exibir 3 links após a página atual
-                for($i = $p + 1; $i <= $p + $max_links; $i++) {
-                    // Verifica se a página atual é maior do que a última página. Se for, não faz nada.
-                    if($i > $pags) {
-                    //faz nada
-                    } else {// Se tiver tudo Ok gera os links.
-                        ?>
-                        <li class="page-item"><a class="page-link" href="listar-filmes.php?p=<?php echo $i;?>"><?php echo $i;?></a></li>
-                        <?php
+							?>
+							<li class="page-item active"><a class="page-link" href="listar-filmes.php?p=<?php echo $p;?>"><?php echo $p;?></a></li>
+							<?php
+                    // Cria outro for(), desta vez para exibir 3 links após a página atual
+                    for($i = $p + 1; $i <= $p + $max_links; $i++) {
+                        // Verifica se a página atual é maior do que a última página. Se for, não faz nada.
+                        if($i > $pags) {
+                        //faz nada
+                        } else {// Se tiver tudo Ok gera os links.
+                            ?>
+                            <li class="page-item"><a class="page-link" href="listar-filmes.php?p=<?php echo $i;?>"><?php echo $i;?></a></li>
+                            <?php
+                        }
                     }
-                }
-                // Exibe o link "última página"
+                    // Exibe o link "última página"
+							?>
+							<li class="page-item"><a class="page-link" href="listar-filmes.php?p=<?php echo $pags;?>">Fim</a></li>
+							</nav> 
+                <?php 
+                } else { // se não tiver nenhum filme
+                    ?>
+                    <div class="row d-flex justify-content-center mt-3">
+                        <div class="col-md-12">
+                            <h5 class="paragrafo-home text-muted">Ops... Nenhum filme encontrado!</h5>
+                            <img class="mx-auto d-block mt-3" style="width: 50%;" src="img/searching.svg" alt="Nada encontrado" >
+                        </div>
+                    </div> 
+                    <?php
+                } 
                 ?>
-                    <li class="page-item"><a class="page-link" href="listar-filmes.php?p=<?php echo $pags;?>">Fim</a></li>
-                </nav>  
-    
             </div>
         </div>
     </section> 
